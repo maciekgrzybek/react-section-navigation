3bba0073696bf61deb0744f1e7bb2c55d695bf98
Maciek Grzybek <maciekgrzybek1@gmail.com>
2019-06-09 20:09:56 +0100

    Eslint errors

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {Provider, createClient} from 'urql'
+import {Provider, createClient} from 'urql';
 
 const client = createClient({
-  url: 'https://rickandmortyapi.com/graphql/'
-})
+  url: 'https://rickandmortyapi.com/graphql/',
+});
 ReactDOM.render(
-  <Provider value={client}>
-    <App />
-  </Provider>,
-  document.getElementById('root'));
+    <Provider value={client}>
+      <App />
+    </Provider>,
+    document.getElementById('root'));
 
