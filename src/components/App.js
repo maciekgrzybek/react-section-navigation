9b0a1f99e534ab2209380d1b0115ee0ad14e55a5
Maciej Grzybek <maciekgrzybek1@gmail.com>
2020-03-31 08:12:25 +0100

    Add example witj static JSON data

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,15 @@
-import React, {useState, useEffect, createRef} from 'react';
-import {useQuery} from 'urql';
-import gql from 'graphql-tag';
+import React, { useState, useEffect, createRef } from 'react';
 
 import './app.css';
 import 'typeface-roboto-mono';
+import { characters } from '../data.json';
 
-import {Navigation} from './Navigation';
-import {Loading} from './Loading';
-import {Character} from './Character';
-import {Footer} from './Footer';
-
-const getCharacters = gql`
-  query AllCharacters{
-    characters(filter: {name: "rick"}) {
-      info {
-        count
-      }
-      results {
-        id
-        name
-        image
-        species
-        status
-        location {
-          name
-        }
-        origin {
-          dimension
-        }
-      }
-    }
-  }
-`;
+import { Navigation } from './Navigation';
+import { Loading } from './Loading';
+import { Character } from './Character';
+import { Footer } from './Footer';
 
 function App() {
-  const [res] = useQuery({
-    query: getCharacters,
-  });
-
   const [activeCharacter, setActiveCharacter] = useState();
   const [pageHeight, setPageHeight] = useState();
 
@@ -50,13 +22,11 @@ function App() {
     });
   }, []);
 
-  if (res.fetching || typeof res.data === 'undefined') {
-    return (
-      <Loading />
-    );
+  if (!characters) {
+    return <Loading />;
   } else {
-    const characters = res.data.characters.results.slice(0, 9);
-    const refs = characters.reduce((refsObj, character) => {
+    const charactersToRender = characters.results.slice(0, 9);
+    const refs = charactersToRender.reduce((refsObj, character) => {
       refsObj[character.name] = createRef();
       return refsObj;
     }, {});
@@ -71,33 +41,50 @@ function App() {
         <div className="page-wrapper">
           <aside className="sidebar">
             <Navigation
-              items={characters}
+              items={charactersToRender}
               activeCharacter={activeCharacter}
-              handleCLick={handleCLick}/>
+              handleCLick={handleCLick}
+            />
           </aside>
           <div className="content">
             <div className="page-intro">
               <h1 className="page-title">Check out these cool Rick&apos;s!</h1>
-              <p>This is an example of using Intersection
-                Observer API with React.<br />
-                I&apos;ve created this simple app to show how you can create side
-                navigation that will detect current visible section.
-                Read more on <a href="https://dev.to/maciekgrzybek/create-section-navigation-with-react-and-intersection-observer-fg0" target="blank">dev.to</a> and <a href="https://medium.com/@maciekgrzybek/create-section-navigation-with-react-and-intersection-observer-b01c5085307d">medium.com</a> or check out the <a href="https://github.com/maciekgrzybek/react-section-navigation">repo</a>.
+              <p>
+                This is an example of using Intersection Observer API with
+                React.
+                <br />
+                I&apos;ve created this simple app to show how you can create
+                side navigation that will detect current visible section. Read
+                more on{' '}
+                <a
+                  href="https://dev.to/maciekgrzybek/create-section-navigation-with-react-and-intersection-observer-fg0"
+                  target="blank"
+                >
+                  dev.to
+                </a>{' '}
+                and{' '}
+                <a href="https://medium.com/@maciekgrzybek/create-section-navigation-with-react-and-intersection-observer-b01c5085307d">
+                  medium.com
+                </a>{' '}
+                or check out the{' '}
+                <a href="https://github.com/maciekgrzybek/react-section-navigation">
+                  repo
+                </a>
+                .
               </p>
             </div>
-            {
-              characters.map((item) => {
-                return (
-                  <Character
-                    key={item.name}
-                    activeCharacter={activeCharacter}
-                    data={item}
-                    setActiveCharacter={setActiveCharacter}
-                    pageHeight={pageHeight}
-                    refs={refs}/>
-                );
-              })
-            }
+            {charactersToRender.map((item) => {
+              return (
+                <Character
+                  key={item.name}
+                  activeCharacter={activeCharacter}
+                  data={item}
+                  setActiveCharacter={setActiveCharacter}
+                  pageHeight={pageHeight}
+                  refs={refs}
+                />
+              );
+            })}
           </div>
         </div>
         <Footer />
--- src/index.js
+++ src/index.js
@@ -4,14 +4,5 @@ import 'intersection-observer';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {Provider, createClient} from 'urql';
 
-const client = createClient({
-  url: 'https://rickandmortyapi.com/graphql/',
-});
-ReactDOM.render(
-    <Provider value={client}>
-      <App />
-    </Provider>,
-    document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById('root'));
